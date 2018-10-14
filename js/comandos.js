854dca23963c41dd49c7a60343fdce07a19a9c89
Felipe <felipe.lm@hotmail.com.br>
2018-10-14 13:31:13 -0300

    felipe
    ajuste

--- js/comandos.js
+++ js/comandos.js
@@ -4,14 +4,14 @@ var alarmesArray = [];
 
 
 //Função para recuperar o estado atual do componetes
-function recuperarVlr(){
+function recuperarVlr() {
     //Função para recuperar os alarmes
     $.ajax({
         url: "php/alarmes.php",
         type: "POST",
         dataType: "json",
         contentType: "application/json; charset=utf-8",
-        success: function(data) {
+        success: function (data) {
             //Pega o array vindo do json
             alarmesArray = data;
             console.group("Alarmes");
@@ -20,19 +20,25 @@ function recuperarVlr(){
                 var n = i + 1;
                 var h = alarmesArray[i]["hora"];
                 var m = alarmesArray[i]["minuto"];
-                if(n < 10){ n = "0" + n; }
-                if(h < 10){ h = "0" + h; }
-                if(m < 10){ m = "0" + m; }
+                if (n < 10) {
+                    n = "0" + n;
+                }
+                if (h < 10) {
+                    h = "0" + h;
+                }
+                if (m < 10) {
+                    m = "0" + m;
+                }
                 console.info("Alarme " + n + " - " + h + ":" + m);
             }
             console.groupEnd("Alarmes");
         },
-        error: function() {
+        error: function () {
             console.log("Erro AJAX - Alarmes");
             return false;
         }
     });
-       
+
     return true;
 }
 
@@ -40,12 +46,14 @@ function recuperarVlr(){
 function cmd_Atualizar() {
     escrever("Atualizando sistema em 5 segundos...");
     falar("Atualizando sistema em 5 segundos...");
-    setTimeout(function(){ location.reload(); }, 5000);
+    setTimeout(function () {
+        location.reload();
+    }, 5000);
     return true;
 }
 
 //Gerar Uma cor
-function cmd_MudarCor(){
+function cmd_MudarCor() {
     if (databaseArray["alarmando"] == true) {
         document.getElementById("logo").style.backgroundColor = "#FF0000";
         document.getElementById("txtnovo").style.color = "#FF0000";
@@ -55,32 +63,32 @@ function cmd_MudarCor(){
         var cor = '#';
 
         // Pega um número aleatório no array acima
-        for (var i = 0; i < 6; i++ ) {
-        //E concatena à variável cor
+        for (var i = 0; i < 6; i++) {
+            //E concatena à variável cor
             cor += hexadecimais[Math.floor(Math.random() * 16)];
         }
-        
+
         document.getElementById("logo").style.backgroundColor = cor;
         document.getElementById("txtnovo").style.color = cor;
     }
-    
+
 }
 
 function cmd_Ola() {
-    
+
     var vlr = Math.floor(Math.random() * 5);
-    
-    switch(vlr) {
+
+    switch (vlr) {
         case 0:
             escrever("Oi, tudo bem?");
             falar("Oi, tudo bem?");
             break;
-            
+
         case 1:
             escrever("Olá");
             falar("Olá");
             break;
-            
+
         case 2:
             escrever("Estou ouvindo");
             falar("Estou ouvindo");
@@ -96,7 +104,7 @@ function cmd_Ola() {
             falar("Precisa de algo?");
             break;
     }
-    
+
 }
 
 //Controles de Musica
@@ -107,7 +115,7 @@ function cmd_LigarMusica() {
 
 function cmd_DesLigarMusica() {
     document.getElementById('webvideo').style.opacity = "0.0";
-    setTimeout(function(){ 
+    setTimeout(function () {
         document.getElementById('webvideo').src = "";
     }, 1000);
 }
@@ -120,27 +128,27 @@ function cmd_RelatorioGeral() {
 
 //Controles de LUZ
 function cmd_LigarLuz() {
-    if(databaseArray["luz"] == true){
+    if (databaseArray["luz"] == true) {
         //Se estiver ligada
         escrever("Luz já está ligada...");
         falar("Luz já está ligada...");
         return false;
     } else {
         //Se estiver desligada
-        alterarDados(true,"");
+        alterarDados(true, "");
         escrever("Ok, ligando a luz...");
         falar("Ok, ligando a luz...");
-                
+
         return true;
     }
 }
 function cmd_DesligarLuz() {
-    if(databaseArray["luz"] == true){
+    if (databaseArray["luz"] == true) {
         //Se estiver ligada
-        alterarDados(false,"");
+        alterarDados(false, "");
         escrever("Ok, apagando a luz...");
         falar("Ok, apagando a luz...");
-                
+
         return true;
     } else {
         //Se estiver desligada
@@ -162,27 +170,27 @@ function cmd_DesligarLuzes() {
 
 //Comandos de Alarme
 function cmd_Alarmar() {
-    
+
     //window.open("https://youtu.be/yFeh1dN9K1s", "_blank");
     if (databaseArray["alarmando"] == false) {
         //Atualizar banco
-        alterarDados("",true);
-    }   
-            
+        alterarDados("", true);
+    }
+
     //Janela de POP UP
     document.getElementById("interact-box").style.display = "block";
     document.getElementById("mp3").volume = 1.0;
     document.getElementById("mp3").play();
-    
+
     cmd_MudarCor();
-    
+
     return true;
 }
 
 function cmd_PararAlarme() {
     if (databaseArray["alarmando"] == true) {
         clearInterval(temp);
-        alterarDados("",false);
+        alterarDados("", false);
         document.getElementById("interact-box").style.display = "none";
         document.getElementById("mp3").pause();
     } else {
@@ -196,26 +204,43 @@ function cmd_PararAlarme() {
 function cmd_Pesquisar(array) {
     var txt = "";
     var url = "https://www.google.com.br/search?q=";
-    
+
+    var cntAndrd = 0;
+
     for (i = 0; i < array.length; i++) {
-        if(array[i] != "" && array[i] != " " && array[i] != "pesquisar" && array[i] != "Pesquisar"){
-            url += array[i] + "%20";
-            txt += array[i] + " ";
+        if (array[i] == "Android") {
+            if (cntAndrd >= 1) {
+                if (array[i] != "" && array[i] != " " && array[i] != "pesquisar" && array[i] != "Pesquisar") {
+                    url += array[i] + "%20";
+                    txt += array[i] + " ";
+                }
+            } else {
+                cntAndrd = cntAndrd + 1;
+            }
+
+        } else {
+            if (array[i] != "" && array[i] != " " && array[i] != "pesquisar" && array[i] != "Pesquisar") {
+                url += array[i] + "%20";
+                txt += array[i] + " ";
+            }
         }
+
     }
-    
+
     escrever("Pesquisando : " + txt);
     falar("Pesquisando " + txt);
-    
-    setTimeout(function(){ window.open(url, "_blank"); }, 1000);
-    
+
+    setTimeout(function () {
+        window.open(url, "_blank");
+    }, 1000);
+
     return true;
 }
 
 //Comandos de Data e Hora
 function cmd_Data() {
     now = new Date();
-    
+
     var sem = [
         "Domingo",
         "Segunda",
@@ -225,7 +250,7 @@ function cmd_Data() {
         "Sexta",
         "Sábado"
     ];
-    
+
     var mes = [
         "Janeiro",
         "Fevereiro",
@@ -240,25 +265,25 @@ function cmd_Data() {
         "Novembro",
         "Dezembro"
     ];
-    
+
     var dian = now.getDate();
-    if(dian < 9) {
+    if (dian < 9) {
         dian = "0" + dian;
     }
-    
+
     var mesn = now.getMonth() + 1;
-    if(mesn < 9) {
+    if (mesn < 9) {
         mesn = "0" + mesn;
     }
-    
+
     escrever("Data - " + dian + "/" + mesn + "/" + now.getFullYear());
-    falar("Hoje é " + sem[now.getDay()] + ", dia " + now.getDate() + " do mês de "  + mes[now.getMonth()] +  " no ano de " + now.getFullYear());
+    falar("Hoje é " + sem[now.getDay()] + ", dia " + now.getDate() + " do mês de " + mes[now.getMonth()] + " no ano de " + now.getFullYear());
     return true;
 }
 function cmd_Hora() {
     now = new Date();
     var min = now.getMinutes();
-    if(min < 9) {
+    if (min < 9) {
         min = "0" + min;
     }
     escrever("Hora - " + now.getHours() + ":" + min);
